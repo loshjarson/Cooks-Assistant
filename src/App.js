afb0ffe1d5797b1772da9998ed5d83368847de3c
loshjarson <jlarson129@outlook.com>
2023-05-21 15:30:57 -0400

    Routes skeleton built out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+//Views
+import Login from './views/login';
+import Home from './views/home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' Component={Login}/>
+        <Route path='/home' Component={Home}/>
+      </Routes>
+    </Router>
   );
 }
 
