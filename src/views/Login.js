bfe07d1e0e33fe25064fdf8521c21eca61baa5aa
loshjarson <jlarson129@outlook.com>
2023-05-24 13:55:21 -0700

    mui to antd

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,40 +1,48 @@
-import { Box, InputLabel, OutlinedInput, FormControl, Button } from "@mui/material";
-import { Col, Row } from "antd";
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Form, Button, Input } from "antd";
 import { useState } from "react";
+
 
 function Login() {
     const [user, setUser] = useState({username:"",password:""})
+    const [form] = Form.useForm();
+
+    const handleChange = (e) => {
+        const {id, value} = e.target
+        console.log(id,value)
+        setUser({...user, [id]:value})
+    }
+
+
+
     return (
         <div style={{textAlign:"center"}}>
-            <header style={{textAlign:"center", borderBottom:"1px solid grey", margin:"0 10px"}}><h1>Login</h1></header>
-            <Box component="form" noValidate margin="25px">
-                <Row gutter={[16,16]}>
-                    <Col span={23} justify="center">
-                        <FormControl sx={{ m: 1 }} variant="outlined" fullWidth>
-                            <InputLabel htmlFor="username">Username</InputLabel>
-                            <OutlinedInput
-                                required
-                                id="username"
-                                label="Username"
-                                onChange={(e)=>setUser({...user,username:e.target.value})}
-                            />
-                        </FormControl>
-                    </Col>
-                    <Col span={23} justify="center">
-                        <FormControl sx={{ m: 1 }} variant="outlined" fullWidth>
-                            <InputLabel htmlFor="password">Password</InputLabel>
-                            <OutlinedInput
-                                required
-                                type="password"
-                                id="password"
-                                label="Password"
-                                onChange={(e)=>setUser({...user,password:e.target.value})}
-                            />
-                        </FormControl>
-                    </Col>
-                </Row>
-            </Box>
-            <Button variant="outlined">Login</Button>
+            <header style={{textAlign:"center", borderBottom:"1px solid grey", margin:"0 10px"}}><h1>Signup</h1></header>
+            <Form
+                form={form}
+                style={{margin:"25px"}}
+                
+            >
+                <Form.Item
+                    name={"username"}
+                    rules={[{
+                        required: true,
+                        message: 'A username is required'
+                    }]}
+                >
+                    <Input prefix={<UserOutlined/>} placeholder='username' onChange={handleChange}/>
+                </Form.Item>
+                <Form.Item
+                    name={"password"}
+                    rules={[{
+                        required: true,
+                        message: 'A password is required'
+                    }]}
+                >
+                    <Input.Password prefix={<LockOutlined/>} placeholder='password' onChange={handleChange}/>
+                </Form.Item>
+            </Form>
+            <Button variant="outlined">Submit</Button>
         </div>
     );
 }
