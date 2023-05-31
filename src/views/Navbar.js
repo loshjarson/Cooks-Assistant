b5f9772d2a724b7409e7faf1618561402093c347
loshjarson <jlarson129@gmail.com>
2023-05-31 14:42:26 -0400

    nacbar skeleton

--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -1,11 +1,18 @@
 import { Paper } from "@mui/material";
+import { Button, Input } from "antd";
+const {Search} = Input
 
 function Navbar() {
     return (
         <Paper elevation={12} style={{
             width: "100vw",
-            height: "10vh",
+            height: "6vh",
+            minHeight: "50px",
+            display: "flex",
+            justifyContent: "center",
+            alignContent:"center"
             }}>
+            <Search style={{width:"20vw", margin:"auto"}}/>
         </Paper>
     );
 }
