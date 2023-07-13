3f26117f0597b1a27031ad6386b27514908eee34
loshjarson <jlarson129@outlook.com>
2023-07-13 18:40:40 -0400

    dynamic rendering list and recipe nav buttons

--- src/views/Components/Navbar.js
+++ src/views/Components/Navbar.js
@@ -26,7 +26,10 @@ function Navbar() {
             alignContent:"center"
             }}>
             <Search style={{width:"20vw", margin:"auto -6rem auto auto"}}/>
-            <Button icon={<UnorderedListOutlined/>} type="text" style={{margin:"auto .5rem auto auto"}}>My Lists</Button>
+            {history.location.pathname === "/home" ? 
+                <Button icon={<UnorderedListOutlined/>} type="text" style={{margin:"auto .5rem auto auto"}} onClick={()=>{history.push("/lists"); history.go("/lists")}}>My Lists</Button>
+            :   <Button icon={<UnorderedListOutlined/>} type="text" style={{margin:"auto .5rem auto auto"}} onClick={()=>{history.push("/home"); history.go("/home")}}>My Recipes</Button> }
+
             <Button icon={<LogoutOutlined/>} type="text" style={{margin:"auto 1rem auto .5rem"}} onClick={() => setShowConfirmation(true)}>Logout</Button>
             <Modal open={showConfirmation} onCancel={()=>setShowConfirmation(false)} title="Logout?" okText="Logout" cancelText="Nevermind" onOk={()=>logout()}><p>Are you sure you want to logout</p></Modal>
         </Paper>
