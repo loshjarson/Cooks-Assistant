f9243a36b178576a11757cc90b7956ebb90bc001
loshjarson <jlarson129@outlook.com>
2023-08-04 17:15:39 -0400

    pulled states and functions from recipes comp

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,10 +4,42 @@ import Navbar from "./Components/Navbar";
 import { useEffect, useState } from "react";
 import history from "../history";
 import axios from "axios";
-import MyLists from "./Components/MyLists";
+import SideBar from "./Components/SideBar";
+import {Buffer} from 'buffer'
+
 
 function Home() {
     const [authenticated,setAuthenticated] = useState(false)
+    const [recipes, setRecipes] = useState([]);
+    const [lists, setLists] = useState([])
+    const [open, setOpen] = useState(false)
+
+    const getMyRecipes = () => {
+        axios.get(`http://localhost:8000/recipes/${sessionStorage.getItem("userId")}`, {headers:{'authorization':`bearer ${sessionStorage.getItem("token")}`}})
+            .then(res => {
+                console.log(res.data)
+                res.data.recipes.map((recipe,i) => {
+                    if(recipe.image){
+                        const base64String = Buffer.from(recipe.image).toString('base64');
+                        res.data.recipes[i] = {...recipe._doc, image:base64String} 
+                    }
+                })
+                setRecipes(res.data.recipes)
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    }
+
+    const getMyRecipeLists = () => {
+        axios.get(`http://localhost:8000/recipelists/${sessionStorage.getItem("userId")}`, {headers:{'authorization':`bearer ${sessionStorage.getItem("token")}`}})
+            .then(res => {
+                setLists(res.data.recipeLists)
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    }
 
     useEffect(()=>{
         if(!sessionStorage.getItem("token")){
@@ -27,19 +59,23 @@ function Home() {
                     history.go("/")
                 }
             })
+            getMyRecipes()
+            getMyRecipeLists()
         }
     },[])
 
     return (
         <div>
             
-            <Navbar/>
+            <Navbar setOpen={setOpen} open={open}/>
+            <SideBar open={open}/>
+            <div>
             {authenticated ? 
                 <Routes>
-                    <Route exact path="/home" Component={Recipes}/> 
-                    <Route path="/lists" Component={MyLists}/>
+                    <Route exact path="/home" element={<Recipes getMyRecipes={getMyRecipes} recipes={recipes} setRecipes={setRecipes} lists={lists} setLists={setLists} open={open}/>}/> 
                 </Routes>
             : null}
+            </div>
         </div>
             
     );
