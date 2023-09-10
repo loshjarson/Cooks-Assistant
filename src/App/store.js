d249008bba1ef2fb81a3d026f53b1a7ebb0cfad0
loshjarson <jlarson129@outlook.com>
2023-09-10 16:51:43 -0400

    persist store

--- src/App/store.js
+++ src/App/store.js
@@ -1,4 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {  combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage  from "redux-persist/lib/storage";
+import thunk from "redux-thunk"
+
 import filterReducer from "../Components/slices/filterSlice"; 
 import recipesReducer from "../Components/slices/recipesSlice";
 import listsReducer from "../Components/slices/listsSlice";
@@ -7,16 +11,32 @@ import draggableReducer from "../Components/slices/draggableSlice";
 import authenticatedReducer from "../Components/slices/authenticatedSlice";
 import usersReducer from "../Components/slices/usersSlice";
 import groceriesReducer from "../Components/slices/grocerySlice"
+import colorReducer from "../Components/slices/colorSlice";
+
+
+const reducers = combineReducers({
+    filter: filterReducer,
+    recipes: recipesReducer,
+    lists: listsReducer,
+    modals: modalsReducer,
+    draggable: draggableReducer,
+    authenticated: authenticatedReducer,
+    users: usersReducer,
+    groceries: groceriesReducer,
+    color: colorReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['filter','recipes','modals','groceries'],
+  };
+
+  const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
     reducer: {
-        filter: filterReducer,
-        recipes: recipesReducer,
-        lists: listsReducer,
-        modals: modalsReducer,
-        draggable: draggableReducer,
-        authenticated: authenticatedReducer,
-        users: usersReducer,
-        groceries: groceriesReducer
-    }
+        persistedReducer,   
+    },
+    middleware:[thunk]
 })
